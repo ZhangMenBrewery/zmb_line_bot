9ffbcc3469df8421851cd59eb4c989808b308851
chunkai721 <chunkai721@gmail.com>
2023-10-16 16:53:53 +0800

    revised admin

--- callback/admin.py
+++ callback/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
+from django.db.models import Case, When, Value, IntegerField
 from callback.models import beer, can
-# Register your models here.
 
 class beerAdmin(admin.ModelAdmin):
-    list_display=('id','Style','eName','cName','ABV','IBU','SRM','Keyword','Description'
+    list_display = ('id', 'time', 'Style', 'eName', 'cName', 'ABV', 'IBU', 'SRM', 'Keyword', 'Description')
+    
+    # 使用Case和When來定義排序條件，將time為'停產'的放在最後面
+    ordering = (
+        Case(
+            When(time='停產', then=Value(1)),
+            default=Value(0),
+            output_field=IntegerField()
+        ),
+        'id',
     )
-    ordering=('id',)
 
 class canAdmin(admin.ModelAdmin):
-    list_display=('cName','eName','ABV','NT_330ml','Description'
-    )
-    ordering=('id',)
+    list_display = ('cName', 'eName', 'ABV', 'NT_330ml', 'Description')
+    ordering = ('id',)
 
-admin.site.register(beer,beerAdmin)
-admin.site.register(can,canAdmin)
+admin.site.register(beer, beerAdmin)
+admin.site.register(can, canAdmin)
