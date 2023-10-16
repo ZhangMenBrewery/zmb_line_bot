34cd67a0ea5704c5a1538da2ceb15b95ac79c096
chunkai721 <chunkai721@gmail.com>
2023-10-16 16:49:41 +0800

    test

--- callback/admin.py
+++ callback/admin.py
@@ -3,15 +3,14 @@ from callback.models import beer, can
 # Register your models here.
 
 class beerAdmin(admin.ModelAdmin):
-    list_display=('id','tapNum',
-    'Style','eName','cName','ABV','IBU','SRM','Keyword'
+    list_display=('id','Style','eName','cName','ABV','IBU','SRM','Keyword','Description'
     )
     ordering=('id',)
 
-class beerAdmin(admin.ModelAdmin):
+class canAdmin(admin.ModelAdmin):
     list_display=('cName','eName','ABV','NT_330ml','Description'
     )
     ordering=('id',)
 
 admin.site.register(beer,beerAdmin)
-admin.site.register(can,beerAdmin)
+admin.site.register(can,canAdmin)
